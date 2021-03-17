23ac91f67ef0b03b070298142cfb5c40e09f96e1
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 12:21:36 +0100

    ex04 Storing object properties as the value of the variables

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+// Only change coode below this line
+players = {
+    7: "Muhamed Besic",
+    10: "Miralem Pjanic",
+    11: "Edin Dzeko"
+}
+
+function myFunction(myObj){
+    var playerNumber = players["10"];
+    var player = playerNumber;
+    return player;
+}
+//Only change code above this line
+
+module.exports = {players, myFunction};
