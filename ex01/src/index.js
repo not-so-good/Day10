f23e2e02f836e9f4b6e3c985db5d05df7553d821
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 11:38:16 +0100

    ex01 Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+// Only change coode below this line
+var myPet = {
+    species: "Cat",
+    name: "Sylvester",
+    legs: 4,
+    friends: ['Tweety', 'Porky Pig' ]
+}
+
+function myFunction(myObj){
+    return myObj;
+}
+//Only change code above this line
+
+console.log(myFunction(myPet)); 
+
+module.exports = {myPet, myFunction};
