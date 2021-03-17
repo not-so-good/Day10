47e36d558cc9ccd2d14b93574fd346f23075ddf5
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 14:25:01 +0100

    ex05 Updating object properties

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+// Only change coode below this line
+var school = {
+    name: "Arena",
+    location: "Sarajevo",
+    established: "2020"
+};
+//End of school object
+
+function myFunction(name){
+// Only change coode below this line
+    school.name = name;
+    return school;
+//Only change code above this line
+}
+
+console.log(myFunction("Paragon")); 
+module.exports = {school, myFunction};
