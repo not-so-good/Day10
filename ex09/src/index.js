e3bc9973615c915fbecf25a0819e0eff94ef2db2
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 22:25:13 +0100

    ex09 .hasOwnProperty() mathod

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -10,9 +10,9 @@ function myFunction(myObj, checkProp) {
     //Only change code above this line
 }
 
-myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title");
-myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "song");
-myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "genre");
-myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "actor");
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "song"));
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "genre"));
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "actor"));
     
 module.exports = myFunction;
