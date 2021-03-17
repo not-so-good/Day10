21a72d4105fecf82cd12b33593044a9c4ec2a52d
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 22:45:30 +0100

    ex08 Lookups

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,39 +1,25 @@
 function getUsername(email){
     var username = '';
 // Only change code below this line
-    /*switch(email){
-        case 'user1@mail.com':
-            username = 'user1';
-            break;
-        case 'user2@mail.com':
-            username = 'user2';
-            break;
-        case 'user3@mail.com':
-            username = 'user3';
-            break;
-        case 'user4@mail.com':
-            username = 'user4';
-            break;
-        case 'user5@mail.com':
-            username = 'user5';
-            break;
-        case 'user6@mail.com':
-            username = 'user6';
-            break;
-    }*/
-
-    var lookup = {
-        user1: 'user1@mail.com',
-        user2: 'user2@mail.com',
-        user3: 'user3@mail.com',
-        user4: 'user4@mail.com',
-        user5: 'user5@mail.com',
-        user6: 'user6@mail.com'
-    };
-    username = lookup[email];
+    var username = {};
+        username['user1@mail.com'] = 'user1';
+        username['user2@mail.com'] = 'user2';
+        username['user3@mail.com'] = 'user3';
+        username['user4@mail.com'] = 'user4';
+        username['user5@mail.com'] = 'user5';
+        username['user6@mail.com'] = 'user6';
+    
+    username = username[email];
+    
 // Only change code above this line
     return username;
 }
 
-getUsername("user2@mail.com");
+console.log(getUsername("user1@mail.com"));
+console.log(getUsername("user2@mail.com"));
+console.log(getUsername("user3@mail.com"));
+console.log(getUsername("user4@mail.com"));
+console.log(getUsername("user5@mail.com"));
+console.log(getUsername("user6@mail.com"));
+console.log(getUsername(""));
 module.exports = getUsername;
