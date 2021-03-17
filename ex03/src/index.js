e1cdede5fa01c7423adca19c3f958006c5eba176
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 14:10:07 +0100

    ex03 Bracket notation

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -3,13 +3,13 @@ var myFood = {
     vegetable: "carrot",
     fruit: "orange",
     drink: "water"
-}
+};
 
 function myFunction(myObj){
     var vegetableValue = myFood["vegetable"];
     var fruitValue = myFood["fruit"];
     var drinkValue = myFood["drink"];
-    return {vegetableValue, fruitValue, drinkValue}
+    return {vegetableValue, fruitValue, drinkValue};
 }
 
 //Only change code above this line
