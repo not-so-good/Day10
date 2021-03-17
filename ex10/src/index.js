64b5dce5aae5c76ed527ccb39d6227b151f52e2c
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 19:03:09 +0100

    ex10 Complex objects

---
+++ ex10/src/index.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            relese_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP",
+            },
+            gold: true,
+        },
+        002: {
+            artist: "Rage Against the Machine",
+            title: "Rage Against the Machine",
+            relese_year: 1991,
+            id: 2,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP",
+            },
+        },
+    };
+}
+myFunction();
+module.exports = myFunction;
