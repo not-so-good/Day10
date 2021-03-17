f4b5b8d3347e88eec6d4fe86211634adfc3f8d35
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-17 11:49:28 +0100

    ex02 Dot notation

---
+++ ex02/src/index.js
@@ -0,0 +1,19 @@
+// Only change coode below this line
+var myClothes = {
+    hat: "Fedora",
+    shirt: "Nike",
+    shoes: "Converse"
+}
+
+function myFunction(myObj){
+    var hatValue = myClothes.hat;
+    var shirtValue = myClothes.shirt;
+    var shoeValue = myClothes.shoes;
+    return {hatValue, shirtValue, shoeValue}
+}
+
+//Only change code above this line
+
+console.log(myFunction(myClothes)); 
+
+module.exports = myFunction(myClothes);
